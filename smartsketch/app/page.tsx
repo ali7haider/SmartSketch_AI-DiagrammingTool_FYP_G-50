a85f50c6fe5275cd1b7daa216a81b8fea53e5459
ali7haider <ali7haider.dev@gmail.com>
2024-10-08 00:02:27 +0500

    Install Shadcn UI

--- smartsketch/app/page.tsx
+++ smartsketch/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Welcome To Smart Sketch (AI-Driven Diagramming Tool)</h2>
+      <Button className="bg-red-500">Lets Start</Button>
     </div>
   );
 }
