582de2c483abfcd87b5ab2ae14f2e70646a0f6b3
ali7haider <ali7haider.dev@gmail.com>
2025-01-07 22:25:43 +0500

    feat: add shape options to SidebarOptionBox with new SVG representations

--- smartsketch/app/(routes)/workspace/_components/SidebarOptionBox.tsx
+++ smartsketch/app/(routes)/workspace/_components/SidebarOptionBox.tsx
@@ -121,6 +121,259 @@ const SidebarOptionBox: React.FC = () => {
             </svg>
           ),
         },
+        {
+          label: "Square",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "32px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <g transform="translate(0.5,0.5)">
+                <rect
+                  x="2.4"
+                  y="1.4"
+                  width="27.2"
+                  height="27.2"
+                  fill="#f1f3f4"
+                  stroke="#000000"
+                  strokeWidth="1.3"
+                />
+              </g>
+            </svg>
+          ),
+        },
+        {
+          label: "Circle",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "32px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <g transform="translate(0.5,0.5)">
+                <ellipse
+                  cx="16"
+                  cy="15"
+                  rx="13.6"
+                  ry="13.6"
+                  fill="#f1f3f4"
+                  stroke="#000000"
+                  strokeWidth="1.3"
+                />
+              </g>
+            </svg>
+          ),
+        },
+        {
+          label: "Process",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "30px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <g transform="translate(0.5,0.5)">
+                <rect
+                  x="1.6"
+                  y="7.8"
+                  width="28.8"
+                  height="14.4"
+                  fill="#f1f3f4"
+                  stroke="#000000"
+                  strokeWidth="1.3"
+                />
+                <path
+                  d="M 4.48 7.8 L 4.48 22.2 M 27.52 7.8 L 27.52 22.2"
+                  fill="none"
+                  stroke="#000000"
+                  strokeWidth="1.3"
+                  strokeMiterlimit="10"
+                />
+              </g>
+            </svg>
+          ),
+        },
+        {
+          label: "Diamond",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "32px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <g transform="translate(0.5,0.5)">
+                <path
+                  d="M 16 1.4 L 29.6 15 L 16 28.6 L 2.4 15 Z"
+                  fill="#f1f3f4"
+                  stroke="#000000"
+                  strokeWidth="1.3"
+                  strokeMiterlimit="10"
+                />
+              </g>
+            </svg>
+          ),
+        },
+        {
+          label: "Parallelogram",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "30px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <g transform="translate(0.5,0.5)">
+                <path
+                  d="M 1.6 22.2 L 6.4 7.8 L 30.4 7.8 L 25.6 22.2 Z"
+                  fill="#f1f3f4"
+                  stroke="#000000"
+                  strokeWidth="1.3"
+                  strokeMiterlimit="10"
+                />
+              </g>
+            </svg>
+          ),
+        },
+        {
+          label: "Dashed Line",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "30px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <line
+                x1="2"
+                y1="28"
+                x2="30"
+                y2="2"
+                stroke="#000000"
+                strokeWidth="1.3"
+                strokeDasharray="4 4"
+              />
+            </svg>
+          ),
+        },
+        {
+          label: "Dotted Line",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "30px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <line
+                x1="2"
+                y1="28"
+                x2="30"
+                y2="2"
+                stroke="#000000"
+                strokeWidth="1.3"
+                strokeDasharray="1.5 3"
+              />
+            </svg>
+          ),
+        },
+        {
+          label: "Line",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "30px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <line
+                x1="2"
+                y1="28"
+                x2="30"
+                y2="2"
+                stroke="#000000"
+                strokeWidth="1.3"
+              />
+            </svg>
+          ),
+        },
+        {
+          label: "Bidirectional Connector",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "30px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <line
+                x1="6"
+                y1="24"
+                x2="26"
+                y2="4"
+                stroke="#000000"
+                strokeWidth="1.3"
+              />
+              <polygon points="5,25 7,23 6,20 8,21" fill="#000000" />
+              <polygon points="27,3 25,5 26,8 24,7" fill="#000000" />
+            </svg>
+          ),
+        },
+        {
+          label: "Directional Connector",
+          svg: (
+            <svg
+              style={{
+                width: "32px",
+                height: "30px",
+                display: "block",
+                position: "relative",
+                pointerEvents: "none",
+              }}
+            >
+              <line
+                x1="5"
+                y1="25"
+                x2="25"
+                y2="5"
+                stroke="#000000"
+                strokeWidth="1.3"
+              />
+              <polygon points="26,4 25,6 24,7 27,5" fill="#000000" />
+            </svg>
+          ),
+        },
       ],
     },
   ];
@@ -148,7 +401,7 @@ const SidebarOptionBox: React.FC = () => {
               {section.shapes.map((shape, index) => (
                 <div
                   key={index}
-                  className="geItem flex items-center justify-centercursor-pointer p-1 hover:bg-gray-100 relative"
+                  className="geItem flex items-center justify-centercursor-pointer p-1 hover:bg-gray-200 relative"
                   style={{ width: "40px", height: "32px" }}
                   onMouseEnter={() => {
                     if (!hoveredShape || hoveredShape.label !== shape.label) {
